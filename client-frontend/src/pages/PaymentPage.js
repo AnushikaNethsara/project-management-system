6acbecf45abdcf0d506fabebc395f92ecbfb26ff
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-05-15 16:53:18 +0530

    Payment Page done / project overview half done
    Payment Page done / project overview half done

--- client-frontend/src/pages/PaymentPage.js
+++ client-frontend/src/pages/PaymentPage.js
@@ -1,11 +1,74 @@
 import React, { Component } from "react";
+import { Button, Box } from "@material-ui/core";
+import bg from "../images/bg.jpg";
 
 class Payment extends Component {
   state = {};
   render() {
     return (
-      <div style={{ marginTop: "30px" }}>
-        <h4>Payment</h4>
+      <div
+        class="  p-3 mb-2  text-white "
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div>
+          <div
+            class="container-lg shadow p-3 mb-5 bg-body rounded text-dark "
+            style={{ marginTop: "6%", backgroundColor: "white" }}
+          >
+            <div style={{ marginTop: "2%" }}>
+              <h3 className="text-center">PAYMENT</h3>
+            </div>
+            <br></br>
+            <div class="container-sm   text-dark ">
+              <h4>Project Title</h4>
+              <p className="text-muted">
+                Mern Stack Project for Student Management System
+              </p>
+
+              <h4>Job Description</h4>
+              <p className="text-muted">
+                Cam voluptatem placeat odit similique. Quis qui nam praesentium
+                corrupti voluptatum voluptatem sed deserunt. Sequi veritatis sit
+                consequatur minus optio. Soluta quia ullam quam illo et aut
+                repellat. Cam voluptatem placeat odit similique. Quis qui nam
+                praesentium corrupti voluptatum voluptatem sed deserunt. Sequi
+                veritatis sit consequatur minus optio. Soluta quia ullam quam
+                illo et aut repellat.Cam voluptatem placeat odit similique. Quis
+                qui nam praesentium corrupti voluptatum voluptatem sed deserunt.
+                Sequi veritatis sit consequatur minus optio. Soluta quia ullam
+                quam illo et aut repellat.
+              </p>
+              <h4>Amount should Pay for this job</h4>
+              <h1>$50.99</h1>
+              <div></div>
+              <div style={{ width: "100%" }}>
+                <Box
+                  display="flex"
+                  flexDirection="row-reverse"
+                  p={1}
+                  m={1}
+                  bgcolor="background.paper"
+                >
+                  <Button variant="contained">Proceed to Pay</Button>
+                </Box>
+              </div>
+              <br></br>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
       </div>
     );
   }
