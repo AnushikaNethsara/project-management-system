6c69d6ccf487f96314ebac610e90b324acb13d0c
AnushikaNethsara <anushikanethsara@gmail.com>
2021-05-28 09:03:14 +0530

    explore page

--- client-frontend/src/pages/Explore.js
+++ client-frontend/src/pages/Explore.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
 import { Link } from "react-router-dom";
@@ -7,64 +7,47 @@ import axios from "axios";
 
 import constants from "../constants/constants";
 import MyProjectCard from "../components/MyProjectCard/MyProjectCard";
+import BeatLoader from "react-spinners/BeatLoader";
+import MyCard from "../components/Card/Card";
 
 class Explore extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { projects: [], name: "" };
-    this.getUserName = this.getUserName.bind(this);
+    this.state = { projects: [], name: "", loading: false };
   }
 
-  //get the list of projects from database
   componentDidMount() {
-    //getting all the projects and puting in the projects array we declared in the constructor
     axios
       .get(constants.backend_url + "/project/")
-      .then((res) => this.setState({ projects: res.data }))
+      .then((res) => {
+        this.setState({ projects: res.data });
+        this.setState({
+          loading: true,
+        });
+      })
       .catch((err) => {
         console.log(err);
       });
-    this.getUserName();
-  }
-
-  getUserName() {
-    var userId = localStorage.getItem("auth-id");
-
-    axios
-      .get(constants.backend_url + "/users/get-user/" + userId)
-      .then((response) => {
-        this.setState({
-          name: response.data[0].name,
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  projectList() {
-    return this.state.projects.map((currentproject) => {
-      return (
-        <MyProjectCard
-          name={this.state.name}
-          project={currentproject}
-          deleteProject={this.deleteProject}
-          key={currentproject._id}
-        />
-      );
-    });
   }
 
   render() {
     return (
       <div style={{ marginTop: "40px" }}>
         <Container>
-          <h4 className="text-center">Explore Projects</h4>
-
-          <br></br>
-          {this.projectList()}
-          <br></br>
+          <h1 className="text-center text-uppercase mt-5">Explore Projects</h1>
+          {this.state.loading ? (
+            <Row>
+              {this.state.projects &&
+                this.state.projects.map((item) => {
+                  return <MyCard data={item} />;
+                })}
+            </Row>
+          ) : (
+            <div className="text-center" style={{ marginTop: "10%" }}>
+              <BeatLoader color={"#0052d4"} loading={true} size={100} />
+            </div>
+          )}
         </Container>
       </div>
     );
